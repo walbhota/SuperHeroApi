6eca7cedc037fb3acdf35e56099579fe41dc028b
j8 <walbhota@gmail.com>
2022-07-19 16:44:50 +0100

    Added the call methods

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -15,7 +15,32 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Clark",
                     LastName = "Kent",
                     Place = "Metropolis"
-            }
+            },
+
+             new SuperHero {
+                    Id = 2,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+            },
+
+             new SuperHero {
+                    Id = 3,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
+            },
+
+             new SuperHero
+             {
+                 Id = 4,
+                 Name = "Doctor Strange",
+                 FirstName = "Stephen",
+                 LastName = "Strange",
+                 Place = "New York City"
+             }
         };
 
 
@@ -25,6 +50,15 @@ namespace SuperHeroAPI.Controllers
             return Ok(heroes);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            return Ok(hero);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
@@ -32,5 +66,30 @@ namespace SuperHeroAPI.Controllers
             return Ok(heroes);
         }
 
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null || hero.Id == 0)
+                return BadRequest("Hero you're trying to update, not found");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(heroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("The Hero you're trying to delete is not found");
+
+            heroes.Remove(hero);
+            return Ok(heroes);
+        }
     }
 }
