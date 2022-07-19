d131f204d8835b61fe754af843b7287eaf72b1ea
j8 <walbhota@gmail.com>
2022-07-19 01:55:14 +0100

    Added the model, controller, get and post methods

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>   
+         {
+             new SuperHero {
+                    Id = 1,
+                    Name = "Super Man",
+                    FirstName = "Clark",
+                    LastName = "Kent",
+                    Place = "Metropolis"
+            }
+        };
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+    }
+}
